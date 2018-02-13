a08b1be727a9d4377128e9335691d144f1484040
Aaron Abromowitz <aabromowitz@gmail.com>
2018-02-12 19:19:54 -0800

    Finished up Firebase work

--- mindsweep.js
+++ mindsweep.js
@@ -1,64 +1,98 @@
-console.log("hey, js has loaded!");
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDas9q5A0HIjvBVZIskc4sZ_doMoRCGhTI",
+    authDomain: "mindsweep-3173a.firebaseapp.com",
+    databaseURL: "https://mindsweep-3173a.firebaseio.com",
+    projectId: "mindsweep-3173a",
+    storageBucket: "",
+    messagingSenderId: "185792539203"
+};
+firebase.initializeApp(config);
+
+// Create global variables.
+let gridCells = [];
+let globalUser;
 
 // Convert HTML elements to JS objects
 let startElem = document.getElementById("start");
 let messageElem = document.getElementById("message");
 let gridElem = document.getElementById("grid");
+let nameElem = document.getElementById("name");
+let winsElem = document.getElementById("wins");
+
+// Create instance of Firebase's GitHub Authentication provider object
+let githubAuth = new firebase.auth.GithubAuthProvider();
+
+// Use Firebase with GitHub Authentication to log in the user
+firebase.auth().signInWithPopup(githubAuth).catch(function(error) {
+    // Log any errors to the console
+    console.log(error);
+});
+
+// When user logs in or logs out:
+firebase.auth().onAuthStateChanged(function(user) {
+    handleAuthStateChange(user, nameElem);
+});
+
+function handleAuthStateChange(user, nameElem) {
+    globalUser = user;
+    let currentUserID = user.uid;
+    let currentUserName = user.displayName
+    let userRef = firebase.database().ref('users/' + currentUserID);
+    userRef.on("value", initializeUser);
+    function initializeUser (dataSnapshot) {
+        // If the user isn't in the database already, add them.
+        if (!dataSnapshot.exists()) {
+        userRef.set({
+            name: currentUserName,
+            wins: 0
+        });
+        }
+
+        // Display the name on the web page.
+        nameElem.value = user.displayName; 
+
+        // Display their number of wins.
+        winsElem.value = dataSnapshot.val().wins;
+
+        // Set the number of wins to the global user variable.
+        globalUser.wins = dataSnapshot.val().wins;
+    }
+}
 
 // Set up event listener to generate the grid.
 startElem.addEventListener("click", generateGrid);
 
-// Create an object that is a grid.
-// It is created outside of the generateGrid function so that it can be referenced in other functions.
-let gridCells = [];
-
 function generateGrid () {
-    console.log("generateGrid entered")
+    // Reset the grid.
+    gridCells = [];
 
-    // First, get the values that were submitted by the user.
+    // Remove the HTML grid.
+    var myNode = document.getElementById("grid");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+
+    // Reset the message.
+    messageElem.textContent = "";
+
+    // Get the values that were submitted by the user.
     let difficultyLevel = document.getElementById("difficulty").value;
     let gridHeight = document.getElementById("rowNum").value;
     let gridWidth = document.getElementById("colNum").value;
-    console.log("Level of difficulty is " + difficultyLevel);
-    console.log("gridHeight is " + gridHeight);
-    console.log("gridWidth is " + gridWidth);
 
-    // Updating the CSS Grid values.
-    document.documentElement.style.setProperty('--numCols', gridHeight);
-    document.documentElement.style.setProperty('--numRows', gridWidth);
+    // Update the CSS Grid values.
+    document.documentElement.style.setProperty('--numCols', gridWidth);
+    document.documentElement.style.setProperty('--numRows', gridHeight);
 
-    // Output the user's input as text (will become an actual grid later).
-    messageElem.textContent = "You chose " + difficultyLevel + " level of difficulty";
-
+    // Create a grid of the specified height and width.
+    // Each grid element is a button that when clicked is a guess.
     for (let i = 0; i < gridHeight; i++) {
         gridCells[i] = [];// Create the 2nd dimension
         for (let j=0; j < gridWidth; j++) {
-            // This is randomly setting a certain percentage of values to 1 and then everything else to 0.
-            // If the difficult level is 1, it will be 10% 1's, 2 will be 20% 1's, etc.
-            // It maps the 100 - difficultyLevel * 10 percent to the 50% point, 
-            //   so that rounding would get difficultyLevel * 10% 1's.
-            // For example if difficultyLevel is 1, it would want to map .9 to .5.
-            // To do this, you would multiply a random number / 9 * 5, so that .9 would map to .5.
-            gridCells[i][j]=Math.round(Math.random()*5/(10-difficultyLevel));
-            if(gridCells[i][j] === 1) {
-                gridCells[i][j]= 'X';
-            }
-
-            // For each element in the grid, generate a button and place it.
-            let para = document.createElement("button");
-            para.id = i + " " + j;
-            let node = document.createTextNode("+");
-            para.appendChild(node);
-            let element = document.getElementById("grid");
-            element.appendChild(para);
-
-            // Create event listener for the button that was generated.
-            para.addEventListener("click", guessCell);
-
-            // Create an event listener for the right click of the button that was gernated.
-            para.addEventListener("contextmenu", turnCellRed);
+            // Create a grid cell that might have a bomb in it.
+            createGridCell(gridCells, difficultyLevel, i, j);
         }
-
     }
 
     // Populate the cells that don't have bombs (not 'X') with how many bombs they border.
@@ -70,8 +104,35 @@ function generateGrid () {
     console.log(gridCells);
 }
 
-function guessCell () {
-    console.log("guessCell entered");
+function createGridCell(array, difficultyLevel, row, col) {
+    // This is randomly setting a certain percentage of values to 1 and then everything else to 0.
+    // If the difficult level is 1, it will be 10% 1's, 2 will be 20% 1's, etc.
+    // It maps the 100 - difficultyLevel * 10 percent to the 50% point, 
+    //   so that rounding would get difficultyLevel * 10% 1's.
+    // For example if difficultyLevel is 1, it would want to map .9 to .5.
+    // To do this, you would multiply a random number / 9 * 5, so that .9 would map to .5.
+    newVal =Math.round(Math.random()*5/(10-difficultyLevel));
+    if(newVal === 1) {
+        newVal = 'X';
+    }
+    array[row][col] = newVal;
+
+    // For each element in the grid, generate a button and place it.
+    let para = document.createElement("button");
+    para.id = row + " " + col;
+    let node = document.createTextNode("+");
+    para.appendChild(node);
+    let element = document.getElementById("grid");
+    element.appendChild(para);
+
+    // Create event listener for the button that was generated.
+    para.addEventListener("click", guessCell);
+
+    // Create an event listener for the right click of the button that was gernated.
+    para.addEventListener("contextmenu", turnCellRed);
+}
+
+function guessCell() {
 
     // Get the ID of the button element that is clicked.
     let cellID = this.id;
@@ -80,7 +141,6 @@ function guessCell () {
     // First, get the Row and Column values that were entered by the user.
     let rowGuess = parseInt(cellArray[0]); // subtract 1 since grid starts at 0
     let columnGuess = parseInt(cellArray[1]);
-    console.log("Row is " + rowGuess + ", Column is " + columnGuess);
 
     if (gridCells[rowGuess][columnGuess]==='X') {//If they lose
         if (countYs(gridCells) === 0) { // If it's their first turn, don't allow them to lose
@@ -114,19 +174,21 @@ function guessCell () {
 
             // Remove event listeners.
             clearButtonListeners(gridCells);
+
+            // Update the number of wins in the database.
+            let userWinRef = firebase.database().ref('users/' + globalUser.uid + '/wins');
+            userWinRef.set(globalUser.wins + 1);
         }
     }
 }
 
 function determineCellNumber (array, row, column) {
-    console.log("determineCellNumber entered");
-
     // Look at potentially all 8 cells around the cell in question.
     // If the cell is at the edge, it will be either 5 cells.
     // Corners will be 3 cells.
     let bombCount = 0;
-    let arrayWidth = array.length;
-    let arrayHeight = array[0].length;
+    let arrayHeight = array.length;
+    let arrayWidth = array[0].length;
     if(array[row][column]!=="X") { // 0 indicates no bomb, X indicates bomb.
         if(row > 0) {
             bombCount = bombCount + isBomb(array[row-1][column]); //below
@@ -195,8 +257,8 @@ function isSelected (arrayValue) {
 
 function sumGrid (array) {
     let total = 0;
-    let arrayWidth = array.length;
-    let arrayHeight = array[0].length;
+    let arrayHeight = array.length;
+    let arrayWidth = array[0].length;
     for (let i=0; i<arrayHeight; i++) {
         for (let j=0; j<arrayWidth; j++) {
             total = total + isNumber(array[i][j]);
@@ -207,8 +269,8 @@ function sumGrid (array) {
 
 function displayGrid (array) {
     let text = "";
-    let arrayWidth = array.length;
-    let arrayHeight = array[0].length;
+    let arrayHeight = array.length;
+    let arrayWidth = array[0].length;
     for (let i=0; i<arrayHeight; i++) {
         for (let j=0; j<arrayWidth; j++) {
             if (array[i][j] === 'Y') {// already guessed
@@ -223,8 +285,8 @@ function displayGrid (array) {
 }
 
 function clearButtonListeners(array){
-    let arrayWidth = array.length;
-    let arrayHeight = array[0].length;
+    let arrayHeight = array.length;
+    let arrayWidth = array[0].length;
     for (let i=0; i<arrayHeight; i++) {
         for (let j=0; j<arrayWidth; j++) {
             let gridID = i + " " + j;
@@ -235,8 +297,8 @@ function clearButtonListeners(array){
 }
 
 function cascadeZeros(array, row, column) {
-    let arrayWidth = array.length;
-    let arrayHeight = array[0].length;
+    let arrayHeight = array.length;
+    let arrayWidth = array[0].length;
 
     // Fill out the cell number.
     let cellNum = determineCellNumber(gridCells, row, column);
@@ -276,8 +338,8 @@ function cascadeZeros(array, row, column) {
 
 function countYs (array) {
     let total = 0;
-    let arrayWidth = array.length;
-    let arrayHeight = array[0].length;
+    let arrayHeight = array.length;
+    let arrayWidth = array[0].length;
     for (let i=0; i<arrayHeight; i++) {
         for (let j=0; j<arrayWidth; j++) {
             if (array[i][j] === 'Y') {
